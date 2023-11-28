82fc9b9e545bf3793c9149d297cdd343fb0b3c48
Marcelo Melo <melooliveira31@gmail.com>
2023-11-28 16:54:43 +0000

    SignUp Fixed

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,28 +1,59 @@
-import { useState} from "react";
+import axios from 'axios';
+
+import {useState} from 'react';
+
 import {useNavigate} from 'react-router-dom';
 
+const API_URL = "http://localhost:5005";
 
-const SignUpPage = () => {
+function SignUpPage(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [error, setError] = useState("");
 
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [userName,setUserName] = useState("");
-    
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSignUpSubmit = (e) => {
+        // Prevent default actions of the form submission e.g.: refreshing the page
         e.preventDefault();
-        navigate('/login')
+
+        // Create a request body object
+        const requestBody = {email, password, name};
+
+        axios.post(`${API_URL}/auth/signup`, requestBody)
+            .then(()=>{
+                navigate('/login');
+            })
+            .catch((error)=>{
+                const errorDescription = error.response.data.message;
+                setError(errorDescription);
+            })
     }
 
-    return(<div>
-        <form onSubmit={handleSubmit}>
-            <input type="userName" name="userName" value={userName} placeholder="User Name" onChange={(e)=> setUserName(e.target.value)}></input>
-            <input type="email" name="email" value={email} placeholder="Your Email" onChange={(e)=> setEmail(e.target.value)}></input>
-            <input type="password" name="password" value={password} placeholder="Your Password" onChange={(e)=> setPassword(e.target.value)} ></input>
-            <button type="submit">Go</button>
+return(
+    <div>
+        <h1>Sign-up Page</h1>
+        <form onSubmit = {handleSignUpSubmit}>
+            <div> 
+                <label>Email:</label>
+                <input type="email" name="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+            </div>
+            <div> 
+                <label>Password:</label>
+                <input type="password" name="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+            </div>
+            <div> 
+                <label>Username:</label>
+                <input type="text" name="username" value={name} onChange={(e)=> setName(e.target.value)}/>
+            </div>
+            <div>
+                <button type="submit">Sign Up</button>
+            </div>
+            {error && <p>{error}</p>}
         </form>
     </div>)
+
 }
 
 export default SignUpPage;
