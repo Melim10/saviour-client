56b45c36fd2a455da2200771b58643d290164b70
Marcelo Melo <melooliveira31@gmail.com>
2023-11-30 17:07:22 +0000

    New question page updated with skills of the logged user

--- src/pages/NewQuestionPage.jsx
+++ src/pages/NewQuestionPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { AuthContext } from "../Context/auth.context";
 import { useNavigate } from "react-router-dom";
@@ -11,52 +11,86 @@ import { useNavigate } from "react-router-dom";
     answers: [],
   });
  */
-  const NewQuestion = () => {
+const NewQuestion = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [skills, setSkills] = useState("");
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const {user} = useContext(AuthContext);
-    const navigate = useNavigate();
-    console.log(user.skills)
+  const API_URL = "http://localhost:5005";
 
-    const API_URL = "http://localhost:5005/api/questions"
+  useEffect(() => {
+    axios.get(`${API_URL}/api/users/${user._id}`).then((response) => {
+      console.log(response.data.skills);
+      setSkills(response.data.skills);
+      setLoading(false);
+    });
+  }, []);
 
+  const handleSubmit = (e) => {
+    const requestBody = { postedBy, title, description };
+    e.preventDefault();
 
-    const handleSubmit=(e)=>{
-        const requestBody = {postedBy, title, description}
-        e.preventDefault();
+    console.log("clicked");
+    axios
+      .post(`${API_URL}/api/questions`, requestBody)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-        console.log("clicked")
-        axios.post(API_URL, requestBody)
-        .then(()=>{
-            navigate('/')
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
-
-    return(
+  return (
+    <div>
+      {!loading ? (
         <div className="margin-div">
-            <form className="question-form" onSubmit={handleSubmit}>
-                <div className="question-form-title">
-                    <input placeholder="Question Title" type="text" name="title" value={title} onChange={(e)=> setTitle(e.target.value)} maxLength={35}
-                    style={{width:"20vw"}}/>
-                    <p>{title.length}/35</p>
-                </div>
-                <div className="question-form-description">
-                    <input placeholder="Your question description..." type="text" name="description" value={description} onChange={(e)=> setDescription(e.target.value)} 
-                    style={{width:"60vw", height:"20vh"}} maxLength={500}/>
-                    <p>{description.length}/500</p>
-                </div>
-                <div className="question-form-select">
-                    <p>Skill's Tags</p>
-                    {user.skills}
-                </div>
-                <button style={{width:"200px"}}>Sumbit Question</button>
-                </form>
+          <form className="question-form" onSubmit={handleSubmit}>
+            <div className="question-form-title">
+              <input
+                placeholder="Question Title"
+                type="text"
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                maxLength={35}
+                style={{ width: "20vw" }}
+              />
+              <p>{title.length}/35</p>
+            </div>
+            <div className="question-form-description">
+              <input
+                placeholder="Your question description..."
+                type="text"
+                name="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                style={{ width: "60vw", height: "20vh" }}
+                maxLength={500}
+              />
+              <p>{description.length}/500</p>
+            </div>
+            <div className="question-form-select">
+              <p>Skill's Tags</p>
+              {skills.map((skill) => {
+                return (
+                  <div>
+                    {skill}
+                  </div>
+                );
+              })}
+            </div>
+            <button style={{ width: "200px" }}>Sumbit Question</button>
+          </form>
         </div>
-    )
-  }
+      ) : (
+        <h1>Loading</h1>
+      )}
+    </div>
+  );
+};
 
-  export default NewQuestion;
+export default NewQuestion;
